7bc7f01fdee45f959df810a99d3298ed51c5a9dd
Julia Baumohl <julia.baumohl@emory.edu>
2024-08-31 19:40:14 -0400

    table 1

--- FinalProjectCodePractice.R
+++ FinalProjectCodePractice.R
@@ -1,10 +1,15 @@
-
+setwd("/Users/juliabaumohl/Library/CloudStorage/OneDrive-Emory/EPI 590 R Bootcamp/EPI590R Final Project/covid_testing.rda")
 
 library(tidyverse)
-setwd("/Users/juliabaumohl/Library/CloudStorage/OneDrive-Emory/EPI 590 R Bootcamp/EPI590R Final Project/covid_testing.rda")
 library(gtsummary)
 
 tbl_summary(
 	covid_testing,
 	by = result,
-	include = c(gender,demo_group,age))
+	include = c(gender,age,pan_day),
+label = list(
+	gender ~ "Gender",
+	age ~ "Age (Years)",
+	pan_day ~ "Day after start of pandemic in which specimen was collected"
+),
+missing_text = "Missing")
