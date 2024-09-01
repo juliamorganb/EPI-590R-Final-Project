c4de1158ee9d1050a3240513733e1712c1529c05
Julia Baumohl <julia.baumohl@emory.edu>
2024-08-31 20:17:12 -0400

    trying linear model

--- FinalProjectCodePractice.R
+++ FinalProjectCodePractice.R
@@ -5,11 +5,13 @@ library(gtsummary)
 
 tbl_summary(
 	covid_testing,
-	by = result,
-	include = c(gender,age,pan_day),
+	by = gender,
+	include = c(result,age,pan_day),
 label = list(
-	gender ~ "Gender",
+	result ~ "Test Result",
 	age ~ "Age (Years)",
 	pan_day ~ "Day after start of pandemic in which specimen was collected"
 ),
 missing_text = "Missing")
+
+linear_model <- lm(gender ~ age + pan_day, data=covid_testing)
